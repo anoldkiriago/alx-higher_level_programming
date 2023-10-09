cc96cbbce0a2fc18832ac6c955b9f6152a528513
anoldkiriago <kiriagoarnold@gmail.com>
2023-10-09 11:43:10 -0700

    Bellamy

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 5-no_c.p
+
+def no_c(my_string):
+    """Remove all characters of c and C"""
+    copy [y for y in my_string if y != 'c' and != 'C']
+    return("".join(copy))
+
