4affcc0a92eb258ca0c10ecba5dfe6e26c9e4f42
anoldkiriago <kiriagoarnold@gmail.com>
2023-10-09 12:39:35 -0700

    Arnold

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# 9-max_integer.py
+
+
+def max_integer(my_list=[]):
+    """Find the biggest integer of a list."""
+    if len(my_list) == 0:
+        return (None)
+
+    big = my_list[0]
+    for y in range(len(my_list)):
+        if my_list[y] > big:
+            big = my_list[y]
+
+    return (big)
